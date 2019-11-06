bff54de5371502751c70e30ca85ba7b15e94b67f
Alexandr Kamyshev <sanek655@mail.ru>
2019-11-06 20:55:03 +0700

    Add table view

--- SearchBarComponent/ViewController.swift
+++ SearchBarComponent/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: - Public Properties
+    
+    //MARK: - Private Properties
+    private let tableViewIdentifier = "cell"
+    private var tableView: UITableView!
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUpTableView()
     }
+    
+    //MARK: - Initializers
+    fileprivate func setUpTableView() {
+        tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: tableViewIdentifier)
+        tableView.dataSource = self
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
 
-
+//MARK: - UITableViewDataSource
+extension ViewController : UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: tableViewIdentifier, for: indexPath)
+        cell.textLabel?.text = "Item #\(indexPath.row)"
+        return cell
+    }
+    
 }
 
